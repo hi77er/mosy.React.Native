c7dce9cc878730c11c734e7661b843e7018701ce
kkras <kkrastev@live.com>
2020-02-05 19:58:19 +0200

    small

--- mosy.React.Native/src/screens/MenuScreen.js
+++ mosy.React.Native/src/screens/MenuScreen.js
@@ -1,29 +1,64 @@
 import React, { useRef } from 'react';
 import { SafeAreaView } from 'react-navigation';
-import { StyleSheet } from 'react-native';
+import { View, Dimensions, StyleSheet } from 'react-native';
 import { Text, Button } from 'react-native-elements';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
 import Spacer from '../components/Spacer';
 import OpenTableAccountModal from '../components/modal/OpenTableAccountModal';
 
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+const initialLayout = { width: Dimensions.get('window').width };
+
 const MenuScreen = () => {
-  const openTableAccountModalRef = useRef(null);
+  // const openTableAccountModalRef = useRef(null);
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
 
   const handleOpenTableAccount = () => {
     openTableAccountModalRef.current.show();
   };
 
-  return <SafeAreaView forceInset={{ top: "always" }}>
-    <Spacer>
-      <Text h3>MenuScreen</Text>
-    </Spacer>
-    <Spacer>
-      <Button title="Open table account" onPress={handleOpenTableAccount} />
-    </Spacer>
+  return <View style={{ flex: 1 }}>
+    {/* 
+      <Spacer>
+        <Text h3>MenuScreen</Text>
+      </Spacer> 
+    */}
 
-    <OpenTableAccountModal ref={openTableAccountModalRef} />
-  </SafeAreaView>;
+    {/* 
+      <Spacer>
+        <Button title="Open table account" onPress={handleOpenTableAccount} />
+      </Spacer> 
+    */}
+
+    <TabView
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+      })}
+    />
+
+    {/* <OpenTableAccountModal ref={openTableAccountModalRef} /> */}
+  </View>;
 };
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+});
 
 export default MenuScreen;
