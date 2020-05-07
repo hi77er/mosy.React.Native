61a759b0b4ee7a8d2eef1cd9549cd11b1e4bfd55
d1trix <ivcho93@gmail.com>
2020-05-07 23:02:55 +0300

    starter working on user authorisation

---
+++ mosy.React.Native/src/services/userService.js
@@ -0,0 +1,27 @@
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+import useResponse from '../hooks/useResponse';
+
+const MOSY_WEBAPI_PUBLIC_URL = "https://wsmosy.azurewebsites.net/";
+
+
+const getUser = async () => {
+  const bearerAccessToken = `Bearer ${JSON.parse(await AsyncStorage.getItem("accessTokenSettings")).access_token}`;
+  const req = axios
+    .create({
+      baseURL: MOSY_WEBAPI_PUBLIC_URL,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": bearerAccessToken,
+      },
+    })
+    .post("/api/user/profile");
+  return useResponse(req);
+}
+
+
+
+
+export const userService = {
+  getUser,
+};
